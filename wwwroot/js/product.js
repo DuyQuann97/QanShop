8190493553d442dc9377e4eaffdd89dc818abda6
DQuan97 <ngduyquan97@gmail.com>
2024-07-24 01:18:01 +0700

    updating search by Name at User UI

--- wwwroot/js/product.js
+++ wwwroot/js/product.js
@@ -15,8 +15,8 @@ const RenderProduct = () => {
         success: function (result) {
             if (result.length > 0) {
                 result.forEach((item, index) => {
-                    // Hiển thị Category Áo 'c3db2a52-7b83-4b61-8863-f42d8a06d389'
-                    if (item.categoryId == "c3db2a52-7b83-4b61-8863-f42d8a06d389") {
+                    // Hiển thị Category Áo 'b6ac5592-b3bb-4353-8021-02ead73edcdf'
+                    if (item.categoryId == "b6ac5592-b3bb-4353-8021-02ead73edcdf") {
                         let x = `<div class="swiper-slide">
                                 <div class="product-card position-relative">
                                     <div class="image-holder">
@@ -70,5 +70,32 @@ const RenderProduct = () => {
     });
 }
 
+//Chức năng tìm kiếm sản phẩm
+
+function GetProduct() {
+    var searchKey = $("#inputSearch").val();
+    $.ajax({
+        type: 'GET',
+        url: 'https://localhost:7071/Product/'
+        data: { keyword: searchKey },
+        success: function (data) {
+            console.log(data);
+            //var table = $("#tblCustomers");
+            //table.find("tr:not(:first)").remove();
+            //$.each(customers, function (i, customer) {
+            //    var table = $("#tblCustomers");
+            //    var row = table[0].insertRow(-1);
+            //    $(row).append("<td />");
+            //    $(row).find("td").eq(0).html(customer.CustomerID);
+            //    $(row).append("<td />");
+            //    $(row).find("td").eq(1).html(customer.ContactName);
+            //    $(row).append("<td />");
+            //    $(row).find("td").eq(2).html(customer.City);
+            //    $(row).append("<td />");
+            //    $(row).find("td").eq(3).html(customer.Country);
+            //});
+        }
+    });
+}
 
 
